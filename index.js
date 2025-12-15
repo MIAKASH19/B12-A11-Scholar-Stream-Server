7ebb66a4db1cbefe61d9041003e514464763fa19
Mi Akash <mszharul1990@gmail.com>
2025-12-15 14:50:49 +0600

    Scholar-details Api created

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {         
+  try {
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,19 +31,27 @@ async function run() {
     const userCollection = database.collection("users");
     const scholarshipCollection = database.collection("scholarships");
 
-    app.get("/scholarships", async(req, res)=>{
-        const cursor = scholarshipCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
-
-     app.get("/recent-scholarships", async (req, res) => {
-      const cursor = scholarshipCollection.find().limit(6).sort({ scholarshipPostDate: -1 });
+    app.get("/scholarships", async (req, res) => {
+      const cursor = scholarshipCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    app.get("/scholarship-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await scholarshipCollection.findOne(query);
+      res.send(result);
+    });
 
+    app.get("/recent-scholarships", async (req, res) => {
+      const cursor = scholarshipCollection
+        .find()
+        .limit(6)
+        .sort({ scholarshipPostDate: -1 });
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
